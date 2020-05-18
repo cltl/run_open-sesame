892116c8d720a08bfcff8d18b53d0e00a174a9fe
MartenPostma <martenp@gmail.com>
2020-05-18 13:42:46 +0200

    added command line arguments

--- run_open_sesame.sh
+++ run_open_sesame.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
+if [ $# -eq 0 ];
+    then
+    echo
+    echo "Usage:                  : $0 naf_folder tasks frame_to_info"
+    echo
+    exit -1;
+fi
+
 here=$(pwd)
 
 out_folder="output" # where do you want to store the output?
-naf_folder="/home/postma/multilingual-wiki-event-pipeline/wiki_output/en" # where is the input folder with NAF files?
-tasks="target-frame"
+naf_folder=$1 #where is the input folder with NAF files?
+tasks=$2
+frame_to_info=$3
 sesame_commit="96639aedd24442433365d4d9fca877931fe222e4" # which commit of open sesame did you use?
 input=$here/$out_folder/all_sentences.txt
 output=$here/$out_folder/all_sentences.conll
@@ -15,4 +24,4 @@ cd scripts
 bash prediction_on_unseen_data.sh $input $tasks $output > ../$out_folder/log.out 2> ../$out_folder/log.err
 cd ..
 
-python update_naf_with_open_sesame_output.py --naf_folder=$naf_folder --output_open_sesame=$out_folder --commit= --verbose=1
+python update_naf_with_open_sesame_output.py --naf_folder=$naf_folder --output_open_sesame=$out_folder --path_frame_to_info=$frame_to_info --commit=$sesame_commit --verbose=1
